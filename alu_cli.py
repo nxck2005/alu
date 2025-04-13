3fb1af4acbbe0a881ba859cf8024ba0e8b794b96
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-04-13 20:30:49 +0530

    Add alu_cli

--- alu_cli.py
+++ alu_cli.py
@@ -1,5 +1,26 @@
+import typing
+
+class ALU:
+    
+    # Flags: ZF, CF, OF
+    FLAGS = [0, 0, 0]
+    
+    REGISTERS = [
+        []
+    ]
+        
+    def __init__(self, speed: int = 4, bits: int = 8):
+        self.bits = bits
+        self.speed = speed
+        print(f"ALU initialized. Architecture: {self.bits} bit.")
+        print(f"Running at {self.speed} Hz.")
+
+
 def main():
-    pass
+    speed = 4
+    bits = 8
+    input("Number of bits for ALU: ", speed)
+    alu = ALU(4, 8)
 
 if __name__ == '__main__':
     main()
