77a8d7576cf28ffed1d624c8359d386815c22064
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-04-13 21:23:32 +0530

    Add changing of singular bit in memory

--- alu_cli.py
+++ alu_cli.py
@@ -26,6 +26,7 @@ class ALU:
 class Memory:
     def __init__(self, rows, archsize):
         self.rows = rows
+        self.archsize = archsize
         self.MEMORY = []
         for _ in range(self.rows):
             self.MEMORY.append([0]*archsize)
@@ -38,16 +39,37 @@ class Memory:
         for line in self.MEMORY:
             print(f"{i} {line}")
             
+    def editIndex(self, row, index, new):
+        if new in (0,1) and row <= self.rows and index <= self.archsize:
+            self.MEMORY[row][index] = new
+        else:
+            print("Must be 0 or 1. Not modified.")
+        return
+    
+    def printMemory(self):
+        print("  ", end='')
+        for i in range(self.rows):
+            print(f' {i}', end=" ")
+        print()
+        i = 0
+        for line in self.MEMORY:
+            print(f"{i} {line}")
+            i += 1
+            
 
 def main():
     speed = 4
     bits = 8
-    memsize = 16
-    bits = int(input("Number of bits for ALU: "))
-    speed = int(input("Speed for ALU in Hz: "))
-    memsize = int(input("Rows of memory: "))
+    memsize = 8
+    # bits = int(input("Number of bits for ALU: "))
+    # speed = int(input("Speed for ALU in Hz: "))
+    # memsize = int(input("Rows of memory: "))
     alu = ALU(speed, bits)
     mem = Memory(memsize, bits)
+    
+    mem.editIndex(1, 3, 1)
+    mem.printMemory()
+    
 
 if __name__ == '__main__':
     main()
