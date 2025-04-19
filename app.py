26aa34dd572f9497ca7de656b50c718a30facc82
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-04-20 02:26:08 +0530

    U: Show registers on content page

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from alu import ALU, Memory
+
+alu = ALU(1)
+mem = Memory(40)
 
 app = Flask(__name__)
 
 @app.route('/alu')
-def alu():
-    return render_template("content.html")
+def alu_route():
+    print(alu.REGISTERS)
+    return render_template("content.html", aluObj=alu, memObj=mem)
