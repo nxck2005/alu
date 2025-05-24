3f0b64fddfb9dad6c475fdfef18e74f5767f0039
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-05-24 22:38:17 +0530

    U: add np to mc

--- microcode.py
+++ microcode.py
@@ -1,11 +1,13 @@
 from alu import ALU, Memory
 from helpers import Helper
 import typing
+from numpy import *
 
 # All instructions here
 def NOP():
     pass
 
+# take a value, add it to ax
 def ADD(reg1: list, reg2: list) -> list:
     pass
 
