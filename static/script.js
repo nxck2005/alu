5d5eb9f2b1cef75a5d2473fa7f5dd918cef5360a
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-05-30 22:30:54 +0530

    U: add selectors for buttons

--- static/script.js
+++ static/script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("hello, world")
+
+    const execButton = document.getElementById("execbutton")
+    const pokeAluButton = document.getElementById("pokealubutton")
+    const pokeMemButton = document.getElementById("pokemembutton")
+    const resetalubutton = document.getElementById("resetalubutton")
+    const resetmembutton = document.getElementById("resetmembutton")
+    
 })
